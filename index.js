f8bfe3efc7ca0587fedd16b07f31a975f27eb787
Aditya <aditya.guglani@vinsol.com>
2023-01-30 20:48:49 +0530

    Remove unused code

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-import App from '@app/public/App';
-import {name as appName} from '@app/public/app.json';
-
+  
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+  
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {rootTag: document.getElementById('root'),});
-
+  
 if (__DEV__) {
-    import('./reactotron-config').then(() =>
-      console.log('Reactotron Configured'),
-    );
-  }
-  
-  import {AppRegistry} from 'react-native';
-  import App from './App';
-  import {name as appName} from './app.json';
-  
-  AppRegistry.registerComponent(appName, () => App);
+  import('./reactotron-config').then(() =>
+    console.log('Reactotron Configured'),
+  );
+}
